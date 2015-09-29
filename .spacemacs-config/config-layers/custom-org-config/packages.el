66fd6c7f823f4a3c8be87d90f1057dc7cde38a59
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2015-09-29 11:05:38 +0200

    update settings for spacemacs-0.104.1

--- .spacemacs-config/config-layers/custom-org-config/packages.el
+++ .spacemacs-config/config-layers/custom-org-config/packages.el
@@ -34,7 +34,7 @@ which require an initialization must be listed explicitly in the list.")
     )
   )
 
-(defun custom-org-config/init-org ()
+(defun custom-org-config/post-init-org ()
 ;;   "Initialize my package"
 ;;load exporters for odt and texinfo - new in org 8
   (use-package org
@@ -50,7 +50,6 @@ which require an initialization must be listed explicitly in the list.")
                org-iswitchb
                org-clock-goto
                org-clock-in
-               mu4e
                bh/org-todo
                bh/widen
                bh/clock-in-last-task
@@ -84,9 +83,12 @@ which require an initialization must be listed explicitly in the list.")
       )
     :config
     (progn
-      ;;(require 'org-pelican)
-      ;;(setq org-src-fontify-natively 1)
-
+      ;; set org specific keybindings
+      ;; (add-hook 'org-agenda-mode-hook
+      ;;           '(lambda () (org-defkey org-agenda-mode-map "R" 'org-agenda-refile))
+      ;;           'append)
+      ;; (require 'org-pelican)
+      (setq org-src-fontify-natively 1)
       (setq org-agenda-span 'day)
       (setq org-default-notes-file "~/Dropbox/shahn/org/refile.org")
 
@@ -246,42 +248,6 @@ which require an initialization must be listed explicitly in the list.")
                      "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
                     ("h" "Habit" entry (file "~/Dropbox/shahn/org/refile.org")
                      "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"<%Y-%m-%d %a .+1d/3d>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
-
-      (defun org-mu4e-store-link ()
-      "Store a link to a mu4e query or message."
-      (cond
-       ;; storing links to queries
-       ((eq major-mode 'mu4e-headers-mode)
-        (let* ((query (mu4e-last-query))
-            desc link)
-      (org-store-link-props :type "mu4e" :query query)
-      (setq
-        desc (concat "mu4e:query:" query)
-        link desc)
-      (org-add-link-props :link link :description desc)
-      link))
-        ;; storing links to messages
-      ((eq major-mode 'mu4e-view-mode)
-        (let* ((msg  (mu4e-message-at-point))
-           (msgid   (or (plist-get msg :message-id) "<none>"))
-           (from (car (car (mu4e-message-field msg :from))))
-           (to (car (car (mu4e-message-field msg :to))))
-           (subject (mu4e-message-field msg :subject))
-           link)
-         (setq link (concat "mu4e:msgid:" msgid))
-         (org-store-link-props :type "mu4e" :link link
-                   :message-id msgid)
-         (setq link (concat "mu4e:msgid:" msgid))
-         (org-store-link-props
-          :type "mu4e" :from from :to to :subject subject
-                :message-id msgid)
-
-         (org-add-link-props :link link
-                 :description (funcall org-mu4e-link-desc-func msg))
-         link))))
-
-      (org-add-link-type "mu4e" 'org-mu4e-open)
-      (add-hook 'org-store-link-functions 'org-mu4e-store-link)
 
       ;; Remove empty LOGBOOK drawers on clock out
       (defun bh/remove-empty-drawer-on-clock-out ()
