e6ab52cc0161ec75b7dc8f4055a4a297077f0de5
Sebastian Hahn <sebastian.hahn@gmail.com>
2018-04-02 22:16:34 +0200

    change to gpg2

--- mail/oimap_passret.py
+++ mail/oimap_passret.py
@@ -5,7 +5,7 @@ import subprocess
 def mailpasswd(acct):
     acct = os.path.basename(acct)
     path = "/home/shahn/dotfiles/mail/%s.gpg" % acct
-    args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", path]
+    args = ["gpg2", "--use-agent", "--quiet", "--batch", "-d", path]
     try:
         return subprocess.check_output(args).strip()
     except subprocess.CalledProcessError:
