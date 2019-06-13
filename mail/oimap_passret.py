a28965e42ad42498c8374009f6f2818e604b1463
Sebastian <sebastian.hahn@gmail.com>
2019-06-13 13:54:44 +0200

    Fix path

--- mail/oimap_passret.py
+++ mail/oimap_passret.py
@@ -4,7 +4,7 @@ import subprocess
 
 def mailpasswd(acct):
     acct = os.path.basename(acct)
-    path = "/home/shahn/dotfiles/mail/%s.gpg" % acct
+    path = "/home/shahn/%s.gpg" % acct
     args = ["gpg2", "--use-agent", "--quiet", "--batch", "-d", path]
     try:
         return subprocess.check_output(args).strip()
