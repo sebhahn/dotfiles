c55f557e37d8af330ab0f0ab62def84ce15faf90
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2022-05-25 09:03:19 +0200

    move python functions

--- .spacemacs.d.symlink/layers/my-python/keybindings.el
+++ .spacemacs.d.symlink/layers/my-python/keybindings.el
@@ -1,2 +1,6 @@
 (spacemacs/set-leader-keys-for-major-mode 'python-mode
   "cn" 'my-python/python-execute-file)
+
+(spacemacs/set-leader-keys-for-major-mode 'python-mode
+  "cs" 'send-region-compilation
+  "cS" 'send-region-compilation-dbg)
