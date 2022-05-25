60c70f58f9b6838d7079e9ffd8368a972c8bed2b
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2022-05-25 10:41:54 +0200

    fix key binding

--- .spacemacs.d.symlink/layers/my-python/keybindings.el
+++ .spacemacs.d.symlink/layers/my-python/keybindings.el
@@ -2,5 +2,5 @@
   "cn" 'my-python/python-execute-file)
 
 (spacemacs/set-leader-keys-for-major-mode 'python-mode
-  "cs" 'send-region-compilation
-  "cS" 'send-region-compilation-dbg)
+  "cs" 'my-python/send-region-compilation
+  "cS" 'my-python/send-region-compilation-dbg)
