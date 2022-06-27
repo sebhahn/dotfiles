a54e588579288516b176dc297f9d449505109490
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2022-06-27 15:46:42 +0200

    remove mu4e-meta dependency

--- .spacemacs.d.symlink/layers/my-mu4e/packages.el
+++ .spacemacs.d.symlink/layers/my-mu4e/packages.el
@@ -81,16 +81,6 @@ https://mrs.geo.tuwien.ac.at/")
           mml2015-encrypt-to-self t)))
 
 (defun my-mu4e/pre-init-org ()
-  (if mu4e-org-link-support
-      (with-eval-after-load 'org
-        (require 'mu4e-meta)
-        (if (version<= mu4e-mu-version "1.3.5")
-            (require 'org-mu4e)
-          (require 'mu4e-org))
-        ;; We require mu4e due to an existing bug https://github.com/djcb/mu/issues/1829
-        ;; Note that this bug prevents lazy-loading.
-        (if (version<= mu4e-mu-version "1.4.15")
-            (require 'mu4e))))
   (if mu4e-org-compose-support
       (progn
         (spacemacs/set-leader-keys-for-major-mode 'mu4e-compose-mode
