a859870a509e952ea911a81680e92ecb58563295
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2024-01-01 23:52:05 +0100

    update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,9 +3,14 @@ return {
 		"catppuccin/nvim",
 		lazy = false,
 		name = "catppuccin",
+		priority = 1001,
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("catppuccin")
+			vim.cmd.colorscheme("onedark")
 		end,
 	},
 }
