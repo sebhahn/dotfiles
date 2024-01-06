da2d56e8236b76054bda83a859c1581208c996ba
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2024-01-06 15:17:56 +0100

    update tmux conf

--- uptime.sh
+++ uptime.sh
@@ -1,1 +1,12 @@
-uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/'
+uptime | awk -F'( |,|:)+' '{
+    d=h=m=0;
+    if ($7=="min")
+        m=$6;
+    else {
+        if ($7~/^day/) { d=$6; h=$8; m=$9}
+        else {h=$6;m=$7}
+        }
+    }
+    {
+        printf("up %03d days %02d hours %02d minutes",d+0,h+0,m+0)
+    }'
