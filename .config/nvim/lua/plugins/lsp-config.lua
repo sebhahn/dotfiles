18915be0a5d5d2d2f64b9c5f0e671d771c6cda46
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2024-04-22 21:39:47 +0200

    update nvim config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -33,23 +33,47 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lsp_attach = function(client, bufnr)
+        -- Create your keybindings here...
+      end
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
+      -- Call setup on each LSP server
+      require('mason-lspconfig').setup_handlers({
+          function(server_name)
+            lspconfig[server_name].setup({
+                on_attach = lsp_attach,
+                capabilities = lsp_capabilities,
+            })
+          end
+        })
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      -- Lua LSP settings
+      lspconfig.lua_ls.setup {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {'vim'},
+            },
+          },
+        },
+      }
 
-			lspconfig.pyright.setup({
-				--on_attach = on_attach,
-				capabilities = capabilities,
-				filetypes = { "python" },
-			})
+      -- Lua LSP settings
+      lspconfig.lua_ls.setup {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {'vim'},
+            },
+          },
+        },
+      }
+
 		end,
 	},
 }
