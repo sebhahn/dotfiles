dda4a69ba0df6c02b40ae55b276f651856da2af5
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2024-07-22 20:09:32 +0200

    clean up settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,29 +2,15 @@
 local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
-local config = {}
+local config = wezterm.config_builder()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
-
---config.font = wezterm.font 'Hack Nerd Font Mono'
 config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular' })
-
 config.font_size = 11.0
 
--- config.color_scheme = 'Material (base16)'
--- config.color_scheme = 'MaterialDark'
--- config.color_scheme = 'Bamboo'
 config.color_scheme = "OneHalfDark"
-
-config.hide_tab_bar_if_only_one_tab = true
 config.term = 'xterm-256color'
-config.window_background_opacity = 0.95
+
+config.window_background_opacity = 0.93
 
 config.window_padding = {
   left = 0,
@@ -34,6 +20,8 @@ config.window_padding = {
 }
 
 config.enable_scroll_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 
 config.default_prog = { 'zsh', '-l' }
 
@@ -43,5 +31,4 @@ config.skip_close_confirmation_for_processes_named = { 'bash', 'sh', 'zsh', 'fis
 config.initial_rows = 42
 config.initial_cols = 120
 
--- and finally, return the configuration to wezterm
 return config
