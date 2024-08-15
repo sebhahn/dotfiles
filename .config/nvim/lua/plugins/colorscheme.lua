7848fb65a1dd5fcd1ac5ec40853e8334695804a7
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2024-08-15 17:18:09 +0200

    update nvim config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,69 @@
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	lazy = false,
-	-- 	name = "catppuccin",
-	-- 	priority = 1001,
-	-- },
-	-- {
-	-- 	"olimorris/onedarkpro.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme("onedark")
-	-- 	end,
-	-- },
+  -- {
+  -- 	"catppuccin/nvim",
+  -- 	lazy = false,
+  -- 	name = "catppuccin",
+  -- 	priority = 1001,
+  -- },
   {
-    -- "folke/tokyonight.nvim",
-    "rose-pine/neovim",
+    "navarasu/onedark.nvim",
     lazy = false,
-    priority = 1001,
+    priority = 1000,
+
     config = function()
-      require("rose-pine").setup({
-          dark_variant = "moon",
-		})
-      vim.cmd("colorscheme rose-pine")
-	end,
+      -- Lua
+      require("onedark").setup({
+        -- Main options --
+        style = "dark", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = false, -- Show/hide background
+        term_colors = true, -- Change terminal color as per the selected theme style
+        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+        -- toggle theme style ---
+        toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+        -- Change code style ---
+        -- Options are italic, bold, underline, none
+        -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+        -- code_style = {
+        --   comments = "italic",
+        --   keywords = "none",
+        --   functions = "none",
+        --   strings = "none",
+        --   variables = "none",
+        -- },
+
+        -- Lualine options --
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+        },
+
+        -- Custom Highlights --
+        -- colors = {}, -- Override default colors
+        -- highlights = {}, -- Override highlight groups
+
+        -- Plugins Config --
+        diagnostics = {
+          darker = true, -- darker colors for diagnostic
+          undercurl = true, -- use undercurl instead of underline for diagnostics
+          background = true, -- use background color for virtual text
+        },
+      })
+      require("onedark").load()
+    end,
   },
+  --  {
+  --    -- "folke/tokyonight.nvim",
+  --    "rose-pine/neovim",
+  --    lazy = false,
+  --    priority = 1001,
+  --    config = function()
+  --      require("rose-pine").setup({
+  --          dark_variant = "moon",
+  -- 	})
+  --      vim.cmd("colorscheme rose-pine")
+  -- end,
+  --  },
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,37 +1,36 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
-
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
     lualine.setup({
-        -- options = {
-        --   theme = "catppuccin",
-        -- },
-        sections = {
-          lualine_x = {
-            {
-              "swenv",
-              cond = function()
-                return vim.bo.filetype == "python"
-              end,
-                icon = "",
-                color = { fg = "#8fb55e" },
-            },
-            {
-              lazy_status.updates,
-              cond = lazy_status.has_updates,
-              color = { fg = "#ff9e64" },
-            },
-            { "encoding" },
-            { "fileformat" },
-            { "filetype" },
+      options = {
+        theme = "onedark",
+      },
+      sections = {
+        lualine_x = {
+          {
+            "swenv",
+            cond = function()
+              return vim.bo.filetype == "python"
+            end,
+            icon = "",
+            color = { fg = "#8fb55e" },
           },
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
         },
+      },
     })
   end,
 }
