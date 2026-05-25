b1335f6cb870466caf58aabb03c430e4532bbd83
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2026-05-25 20:21:17 +0200

    add tv layer

---
+++ spacemacs/.spacemacs.d/layers/my-television/keybindings.el
@@ -0,0 +1,6 @@
+(spacemacs/declare-prefix "ot" "television")
+(spacemacs/set-leader-keys
+  "otf" #'my-television-find-file
+  "otl" #'my-television-git-log
+  "otb" #'my-television-git-branch
+  "otp" #'my-television-toggle-preview)
